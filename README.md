ffd39fe32b2e3c9fd4494805b1d74efd02db3350
Eftakhairul Islam <eftakhairul@gmail.com>
2015-11-12 04:19:16 -0500

    basic build script is done

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # NodeJS Tricks
 
 Bringing all the helpful tricks from the different websites, talks, tweets etc in one place. Just fork and contribute this one. 
+```javascript
+console.log('hello world!');
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,28 @@
 // Include gulp
 var gulp = require('gulp');
+var fs   = require('fs');
+var util = require('util');
+
+
+var fileNames  =  [];
+var targetFile =  __dirname + '/' + 'README.md';
+
+gulp.task('content', function() {
+    var files = fs.readdirSync(__dirname);
+
+    //filter .js files except the gulpfile.js
+    files.filter(function(file) { return file.substr(-3) === '.js' && file.toString() !== 'gulpfile.js'})
+         .forEach(function(file) {
+              fileNames.push(file.toString());
+              var fileName = __dirname + '/' + file;
+              var contents = fs.readFileSync(fileName).toString();
+              contents = util.format(['```javascript',
+                                      '%s',
+                                      '```'].join('\n'), contents);
+
+              fs.appendFileSync(targetFile, contents);
+         });
+});
+
+// Default Task
+gulp.task('build', ['content']);
