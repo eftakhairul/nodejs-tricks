6049481e9980d452239697c07e1239fd1de49942
Eftakhairul Islam <eftakhairul@gmail.com>
2015-11-13 05:07:10 -0500

    build file is done

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+#NodeJS Tricks 
+ 	              Bringing all the helpful tricks from the different websites, talks, tweets etc                   in one place. Just fork and contribute this one.
+## Table of Contents
+- [Test Name](#test-name)
+## Tricks
+### Test Name
 ```javascript
 console.log('hello world!');
 ```
+[View Source](test_name.js)
--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,22 @@
 var gulp = require('gulp');
 var fs   = require('fs');
 var util = require('util');
-
+var path = require("path");
 
 var fileNames  =  [];
 var targetFile =  __dirname + '/' + 'README.md';
 
+//helper function
+String.prototype.capitalize = function() {
+    return this.replace(/(?:^|\s)\S/g, function(a) {
+        return a.toUpperCase();
+    });
+};
+
 gulp.task('header', function() {
-    var header = "#NodeJS Tricks \
-	##Bringing all the helpful tricks from the different websites, talks, tweets etc in one place. Just fork and \
-    ##contribute this one." ;
+    var header = "#NodeJS Tricks \n \
+	              Bringing all the helpful tricks from the different websites, talks, tweets etc \
+                  in one place. Just fork and contribute this one.\n" ;
 
     fs.writeFileSync(targetFile, '');
     fs.appendFileSync(targetFile, header);
@@ -18,26 +25,46 @@ gulp.task('header', function() {
 
 gulp.task('content', function() {
     var files = fs.readdirSync(__dirname);
+    var body  = '';
 
     //filter .js files except the gulpfile.js
     files.filter(function(file) { return file.substr(-3) === '.js' && file.toString() !== 'gulpfile.js'})
          .forEach(function(file) {
-              fileNames.push(file.toString());
-              var fileName = __dirname + '/' + file;
-              var contents = fs.readFileSync(fileName).toString();
-              contents = util.format(['```javascript',
-                                      '%s',
-                                      '```'].join('\n'), contents);
+              var fileName = file.toString();
+              fileNames.push(fileName);
 
-              fs.appendFileSync(targetFile, contents);
+              var heading  = path.basename(fileName, '.js').replace('_', ' ').capitalize();
+              var filePath = __dirname + '/' + fileName;
+              var contents = fs.readFileSync(filePath).toString();
+              body         = body + util.format(['### %s',
+                                                '```javascript',
+                                                '%s',
+                                                '```',
+                                                '[View Source](%s)'].join('\n'), heading, contents, fileName) + '\n';
          });
+
+
+    fs.appendFileSync(targetFile, '## Table of Contents\n');
+
+    fileNames.forEach(function(fileName) {
+        var heading  = path.basename(fileName, '.js').replace('_', ' ').capitalize();
+        var link     = path.basename(fileName, '.js').replace('_', '-').toLowerCase();
+        var item     = util.format(['- [%s](#%s)'].join('\n'), heading, link);
+
+        fs.appendFileSync(targetFile,item);
+        fs.appendFileSync(targetFile,'\n');
+    });
+
+    fs.appendFileSync(targetFile, '## Tricks\n');
+    fs.appendFileSync(targetFile, body);
+
 });
 
 gulp.task('contributors', function() {
 });
 
-gulp.task('header', function() {
+gulp.task('footer', function() {
 });
 
 // Default Task
-gulp.task('build', ['content']);
+gulp.task('build', ['header', 'content', 'contributors', 'footer']);
